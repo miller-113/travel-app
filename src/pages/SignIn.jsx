5ed4919b9b4fe61bf14933ca977bb7b9f076d61c
Nikita <nikita.shkuratt@gmail.com>
2024-07-09 23:49:49 +0300

    added signin page

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <h1 className="visually-hidden">Travel App</h1>
+      <form onSubmit={handleSubmit} className="sign-in-form" autoComplete="off">
+        <h2 className="sign-in-form__title">Sign In</h2>
+        <label className="input">
+          <span className="input__heading">Email</span>
+          <input data-test-id="auth-email" name="email" type="email" required />
+        </label>
+        <label className="input">
+          <span className="input__heading">Password</span>
+          <input
+            data-test-id="auth-password"
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            required
+          />
+        </label>
+        <button data-test-id="auth-submit" className="button" type="submit">
+          Sign In
+        </button>
+      </form>
+      <span>
+        Don&apos;t have an account?
+        <Link
+          to="/sign-up"
+          data-test-id="auth-sign-up-link"
+          href="./sign-up.html"
+          className="sign-in-form__link"
+        >
+          Sign Up
+        </Link>
+      </span>
+    </>
+  );
+};
+
+export default SignIn;
