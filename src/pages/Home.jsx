f4e2a2487fdfccbf67fc15841b6c7784660d4905
Nikita <nikita.shkuratt@gmail.com>
2024-07-09 23:42:22 +0300

    + added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+// src/pages/Home.jsx
+import { useState, useEffect } from "react";
+import tripsData from "../data/trips.json";
+import TripCard from "../components/TripCard";
+import TripFilter from "../components/TripFilter";
+
+const Home = () => {
+  const [trips, setTrips] = useState(tripsData);
+
+  useEffect(() => {
+    setTrips(tripsData);
+  }, []);
+
+  return (
+    <>
+      <TripFilter />
+
+      <section className="trips">
+        <h2 className="visually-hidden">Trips List</h2>
+        <ul className="trip-list">
+          {trips.map(trip => (
+            <TripCard key={trip.id} tripDetails={trip}/>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default Home;
