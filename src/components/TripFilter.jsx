5b33aff21726623cfc4ad62e30107caa28180f87
Nikita <nikita.shkuratt@gmail.com>
2024-07-09 22:48:28 +0300

    + added TripFilter component;left todo for later(add events)

---
+++ src/components/TripFilter.jsx
@@ -0,0 +1,42 @@
+const TripFilter = () => {
+  // TODO add action onChange for each section(input)
+  const optionsFilterDuration = ["duration", "&lt; 5 days", "&lt; 10 days", "&ge; 10 days"].map((option, ind) => (
+    <option value={ind === 0 ? "" : option} key={ind}>
+      {option}
+    </option>
+  ));
+  const optionsFilterLevel = ["level", "easy", "moderate", "difficult"].map((option, ind) => (
+    <option value={ind === 0 ? "" : option} key={ind}>
+      {option}
+    </option>
+  ));
+  return (
+    <section className="trips-filter">
+      <h2 className="visually-hidden">Trips filter</h2>
+      <form className="trips-filter__form" autoComplete="off">
+        <label className="trips-filter__search input">
+          <span className="visually-hidden">Search by name</span>
+          <input
+            data-test-id="filter-search"
+            name="search"
+            type="search"
+            placeholder="search by title"
+          />
+        </label>
+        <label className="select">
+          <span className="visually-hidden">Search by duration</span>
+          <select data-test-id="filter-duration" name="duration">
+            {optionsFilterDuration}
+          </select>
+        </label>
+        <label className="select">
+          <span className="visually-hidden">Search by level</span>
+          <select data-test-id="filter-level" name="level">
+            {optionsFilterLevel}
+          </select>
+        </label>
+      </form>
+    </section>
+  );
+};
+export default TripFilter;
