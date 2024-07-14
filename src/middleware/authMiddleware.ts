067c78ab48b544870e123b8004ad0549c41446a4
Nikita <nikita.shkuratt@gmail.com>
2024-07-14 20:17:31 +0300

    + upd for unlogout user when 401 error

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,14 +1,22 @@
 import { createListenerMiddleware, isRejectedWithValue } from "@reduxjs/toolkit";
 import { signOut } from "../features/auth/authThunks";
+import { toast } from "react-toastify";
 
 const authMiddleware = createListenerMiddleware();
 
 authMiddleware.startListening({
   matcher: isRejectedWithValue,
   effect: async (action, listenerApi) => {
-    const status = action.payload?.status;
-    if (status === 401) {
-      listenerApi.dispatch(signOut());
+    const status = action.payload?.message;
+    if (status === "Unauthorized user") {
+
+      toast.error("Session expired. Please log in again.", { className: "notification" });
+      setTimeout(() => {
+        listenerApi.dispatch(signOut());
+        window.location.href = "/travel-app/sign-in";
+      }, 5000)
+    } else {
+      toast.error(action.payload.message || "Unknown error", { className: "notification" });
     }
   },
 });
